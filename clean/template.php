2afb5a6d57f2e098ec8e45c7c5b4147a4bf88469
Liyun Cecil <liyuncecil@gmail.com>
2018-11-01 21:52:01 -0700

    Adds initial application files

---
+++ clean/template.php
@@ -0,0 +1,16 @@
+
+<?php include 'header.php'?>
+// template as a starting point for all pages of a website
+/* 
+this is a functional include file
+config= configuration. It's how the app begins to work, ex: login
+*/
+<p>Clever content goes here!</p>  
+<p>Clever content goes here!</p>  
+<p>Clever content goes here!</p>  
+<p>Clever content goes here!</p>  
+<p>Clever content goes here!</p>  
+<p>Clever content goes here!</p>  
+<p>Clever content goes here!</p>  
+
+<?php include 'footer.php'?>
