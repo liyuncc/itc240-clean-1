1b38405ad1d38bfc786083d2dcb9248f5372cbd8
Liyun Cecil <liyuncecil@gmail.com>
2018-11-09 15:48:45 -0800

    Updates config file, adds page header, daily page

--- clean/config.php
+++ clean/config.php
@@ -2,11 +2,7 @@
 /*
     config.php
     stores configuration data for our application
-
-
-
 */
-
 //echo basename($_SERVER['PHP_SELF']);
 
 
@@ -15,15 +11,29 @@ define('THIS_PAGE', basename($_SERVER['PHP_SELF']));
 //echo 'the constant is storing: ' . THIS_PAGE;
 //die;
 
+//default page values
+$title = THIS_PAGE;
+$siteName = 'Site Name';
+$slogan = 'Whatever it is you do, we do it better!';
+$pageHeader = 'The developer forgot to put a page header';
+$subHeader = 'The developer forgot to put a sub header';
+
 switch(THIS_PAGE){
     case 'template.php':
         $title= 'My template page';
+        $pageHeader = 'Put PageID here';
+        $subHeader = 'Put more info about page here';
+    break;
+        
+    case 'daily.php':
+        $title= 'My daily page';
+        $pageHeader = 'Daily Coffee Specials';
+        $subHeader = 'All our coffee is special!';
     break;
         
     case 'contact.php':
         $title= 'My contact page';
-    break;
-        
-    default:
-        $title= THIS_PAGE;
+        $pageHeader = 'Please contact us';
+        $subHeader = 'We appreciate your feedback';
+    break; 
 }
